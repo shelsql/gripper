acd2a5bfdab3b498f6edc36997c072776be27509
shelsql <shelsql@163.com>
2024-03-31 01:02:28 +0800

    Added SimTrackDataset

--- datasets/ref_dataset.py
+++ datasets/ref_dataset.py
@@ -144,3 +144,94 @@ class SimTestDataset(Dataset):
     def __len__(self):
         return len(self.rgb_paths)
     
+
+class SimTrackDataset(Dataset):
+    def __init__(self,
+                 dataset_location="/root/autodl-tmp/shiqian/code/gripper/test_views/franka_69.4_1024",
+                 use_augs=False,
+                 seqlen=64,
+                 features=False
+                 ):
+        super().__init__()
+        print("Loading reference view dataset...")
+        self.S = seqlen
+        self.features = features
+        self.dataset_location = dataset_location
+        self.rgb_paths = glob.glob(dataset_location + "/*png")
+        self.camera_intrinsic_path = dataset_location + "/camera_intrinsics.json"
+        
+        total_frames = len(self.rgb_paths)
+        print("Found %d frames in %s" % (total_frames, self.dataset_location))
+        self.all_full_idx = []
+        for ii in range(0, max(total_frames-self.S,1), self.S):
+            # print('bbox_areas[%d]' % ii, bbox_areas[ii])
+            full_idx = ii + np.arange(self.S)
+            full_idx = [ij for ij in full_idx if ij < total_frames]
+            if len(full_idx) == self.S:
+                self.all_full_idx.append(full_idx)
+        print("Found %d clips of length %d in %s" % (len(self.all_full_idx), self.S, self.dataset_location))
+    
+        
+    def __getitem__(self, index):
+        
+        full_idx = self.all_full_idx[index]
+        glob_paths = self.rgb_paths[full_idx]
+        rgbs = []
+        depths = []
+        masks = []
+        c2ws = []
+        obj_poses = []
+        if self.features:
+            feats = []
+        
+        camera_intrinsic = json.loads(open(self.camera_intrinsic_path).read())
+        
+        for glob_rgb_path in glob_paths:
+            path = glob_rgb_path[:-8]
+        
+            rgb_path = path + "_rgb.png"
+            depth_path = path + "_depth1.exr"
+            mask_path = path + "_id1.exr"
+            c2w_path = path + "_c2w.npy"
+            obj_pose_path = path + "_objpose.npy"
+            feat_path = path + "_feats.npy"
+            
+            #print(rgb_path)
+            rgb = cv2.cvtColor(cv2.imread(rgb_path), cv2.COLOR_BGR2RGB)
+            depth = cv2.imread(depth_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)[:,:,0:1]
+            mask = cv2.imread(mask_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)[:,:,0:1]
+            c2w = np.load(c2w_path)
+            obj_pose = np.load(obj_pose_path)
+            if self.features:
+                feat = np.load(feat_path)
+                feats.append(feat)
+            
+            rgbs.append(rgb)
+            depths.append(depth)
+            masks.append(mask)
+            c2ws.append(c2w)
+            obj_poses.append(obj_pose)
+        rgbs = np.stack(rgbs, axis = 0)
+        depths = np.stack(depths, axis = 0)
+        masks = np.stack(masks, axis = 0)
+        c2ws = np.stack(c2ws, axis = 0)
+        obj_poses = np.stack(obj_poses, axis = 0)
+        if self.features:
+            feats = np.stack(feats, axis = 0)
+        else:
+            feats = None
+        #print(depths.shape)
+        sample = {
+            "rgbs": rgbs,
+            "depths": depths,
+            "masks": masks,
+            "c2ws": c2ws,
+            "obj_poses": obj_poses,
+            "feats": feats,
+            "intrinsics": camera_intrinsic
+        }
+        
+        return sample
+    def __len__(self):
+        return len(self.all_full_idx)
+    
